bf480e1c32c188ee934574e52de6a33c866939f6
DooMachine <okanaslankan@hotmail.com>
2018-09-19 20:13:13 +0300

    add some buttons

--- src/app/components/redis-instance/redis-instance.component.ts
+++ src/app/components/redis-instance/redis-instance.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { RedisInstance } from '../../models/redis';
 
 @Component({
   selector: 'app-redis-instance',
   templateUrl: './redis-instance.component.html',
-  styleUrls: ['./redis-instance.component.scss']
+  styleUrls: ['./redis-instance.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RedisInstanceComponent implements OnInit {
 
